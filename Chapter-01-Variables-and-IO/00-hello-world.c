f725a90f94824749807e01d907e1c3819126803c
Abhishek Choudhery <229499305+abhishekflow@users.noreply.github.com>
2025-12-05 17:31:10 +0530

    Added: 00-hello-world.c (Basic output program)

---
+++ Chapter-01-Variables-and-IO/00-hello-world.c
@@ -0,0 +1,12 @@
+// Program     : Hello World
+// File        : 00-hello-world.c
+// Author      : Abhishek
+// Topic       : Basic Output
+// Description : Basic C program to print "Hello World" to the screen.
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+    return 0;
+}
